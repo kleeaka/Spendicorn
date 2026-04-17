1747fd61892dd1f9c35403d052c2e7e15a9e862c
kleeaka <kleeaka@yahoo.com>
2026-04-17 11:12:28 -0500

    Update fix-quotes 0416261112.js

--- fix-quotes.js
+++ fix-quotes.js
@@ -3,5 +3,7 @@ let c = readFileSync("src/App.jsx", "utf8");
 c = c.replace(/[\u201c\u201d]/g, String.fromCharCode(34));
 c = c.replace(/[\u2018\u2019]/g, String.fromCharCode(39));
 c = c.replace(/\u2026/g, "...");
+c = c.replace(/```[\s\S]*?```/g, "");
+c = c.replace(/`{3}/g, "");
 writeFileSync("src/App.jsx", c);
 console.log("Quotes fixed");
